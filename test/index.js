270049d3eabbae92ac919b50d375f4d23501b6be
James Kyburz <james.kyburz@gmail.com>
2017-04-07 16:29:43 +0200

    test changes due to api change

--- test/index.js
+++ test/index.js
@@ -3,29 +3,29 @@ const run = require('../')
 
 tape('generator function with callbacks', (t) => {
   run(function * () {
+    t.plan(2)
     const data = yield (cb) => cb(null, 'data!')
     t.equals('data!', data, 'data received by thunk')
-    t.end()
-  }, t.fail)()
+  }, t.error)()
 })
 
 tape('generator function with promises', (t) => {
   run(function * () {
+    t.plan(2)
     const data = yield Promise.resolve('data!')
     t.equals('data!', data, 'data received by promise')
-    t.end()
-  }, t.fail)()
+  }, t.error)()
 })
 
 tape('generator function with a mix', (t) => {
   run(function * () {
+    t.plan(3)
     const promise = yield Promise.resolve('promise data!')
     t.equals('promise data!', promise, 'data received by promise')
 
     const callback = yield (cb) => cb(null, 'callback data!')
     t.equals('callback data!', callback, 'data received by thunk')
-    t.end()
-  }, t.fail)()
+  }, t.error)()
 })
 
 tape('generator handling callback errors', (t) => {
@@ -33,7 +33,7 @@ tape('generator handling callback errors', (t) => {
     t.plan(1)
     yield (cb) => cb(new Error('fail!'))
     t.fail()
-  }, t.pass)()
+  }, (err) => t.ok(err, 'yes it failed'))()
 })
 
 tape('generator handling promise rejections', (t) => {
@@ -41,10 +41,10 @@ tape('generator handling promise rejections', (t) => {
     t.plan(1)
     yield Promise.reject(new Error('fail!'))
     t.fail()
-  }, t.pass)()
+  }, (err) => t.ok(err, 'yes it failed'))()
 })
 
-const t = (fn) => (t) => run(fn, t.fail)(t)
+const t = (fn) => (t) => run(fn, (err) => err ? t.fail() : t.pass())(t)
 
 tape('test generator test :)', t(function * (t) {
   const promise = yield Promise.resolve('promise data!')
